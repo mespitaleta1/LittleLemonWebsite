eeb5220b156017bba4833c98264620f829b654ba
melissa espitaleta <m.espitaleta1@gmail.com>
2024-08-11 17:51:45 -0500

    installing a test dependency and a basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen,fireEvent } from '@testing-library/react';
+import BookingForm from "../src/components/BookingForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders the BookingForm heading', () => {
+  render(<BookingForm />);
+  const headingElement = screen.getByLabelText(/Choose date/);
+  expect(headingElement).toBeInTheDocument();
 });
